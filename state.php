c58a6c4d06ad95563f25c7741a2b037b43839402
Sajjad Ranjbar <sajjadranjbaryazdi@gmail.com>
2022-10-17 16:39:52 +0330

    Update state.php

--- state.php
+++ state.php
@@ -1,4 +1,20 @@
 <?php
 include("db_connect.php");
-
+$errors = [];
+$request = $_SERVER["REQUEST_METHOD"];
+$allowed_request = ["GET", "POST", "PUT", "DELETE"];
+if(isset($_post['student_id']))
+{
+  
+}
+else
+{
+  array_push($errors,"'student_id' parameter is undefined!");
+  $data = array(
+    "success" => false,
+    "http_code" => 400
+  );
+}
+if(count($errors) != 0) $data['$errors'] = $errors;
+echo json_encode($data);
 ?>
