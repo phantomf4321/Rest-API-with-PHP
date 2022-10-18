f685d45465f495e71f6f491d4f7c2c9b969f00f9
Sajjad Ranjbar <sajjadranjbaryazdi@gmail.com>
2022-10-18 21:01:36 +0330

    Update state.php

--- state.php
+++ state.php
@@ -3,35 +3,37 @@ include("db_connect.php");
 $errors = [];
 $request = $_SERVER["REQUEST_METHOD"];
 $allowed_request = ["GET", "POST", "PUT", "DELETE"];
-if(isset($_post['student_id']))
+if(isset($_POST['student_id']))
 {
-  if(in_array($request,$allowed_request)){
+    //echo "hi";
+  if(in_array($request,$allowed_request))
+  {
     if($request == 'POST')
     {
+      //echo "h2";
       $student_id = $_POST['student_id']; //Registered id of the student in the device
       $date = $_POST['date']; //Recorded date YYYY/MM/DD
       $time = $_POST['time']; //Recorded hours H:i:s
       $type = $_POST['type']; //type of traffic (entry/exit)
+      $device_id = $_POST['device_id']; //Registered id of the device
       
-      $sql = "INSERT INTO employee (name, day, time)
-      VALUES ($student_id, $date, $time)";
+      $sql = "INSERT INTO en_ex (student_id, date, time, type, device_id)
+      VALUES ($student_id, '$date', '$time', $type, $device_id)";
 
-      if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
+      if ($conn->query($sql) === TRUE)
+      {
+        array_push($errors,"New record created successfully");
+        $data = array(
+          "success" => true,
+          "http_code" => 200
+        );
       } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
       }
     }
     
-  }else
-    array_push($errors,"'your http request is ilegal!");
-    $data = array(
-      "success" => false,
-      "http_code" => 400
-    );
   }
-  
-}
+ }
 else{
   array_push($errors,"'student_id' parameter is undefined!");
   $data = array(
