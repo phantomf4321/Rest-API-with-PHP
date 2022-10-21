cfdc799c2978fa8ad8fb54b461656ce91fdad35a
Sajjad Ranjbar <sajjadranjbaryazdi@gmail.com>
2022-10-21 12:23:36 +0330

    Update Request.py

--- POST Request/Request.py
+++ POST Request/Request.py
@@ -12,17 +12,14 @@ device_id=8
 time="888"
 date="128"
 t=8
-
 # data to be sent to api
 data = {'student_id':student_id,
         'device_id':device_id,
         'time':time,
         'date':date,
         'type':t  }
-
 # sending post request and saving response as response object
 r = requests.post(url = API_ENDPOINT, data = data)
-
 # extracting response text
 pastebin_url = r.text
 print("The pastebin URL is:%s"%pastebin_url)
