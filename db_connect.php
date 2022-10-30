cedd4d1d9e63ebd203664cc73ce6b76933532a8f
Sajjad Ranjbar <sajjadranjbaryazdi@gmail.com>
2022-10-30 15:57:22 +0330

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -2,10 +2,8 @@
 $servername = "localhost";
 $username = "username";
 $password = "password";
-
 // Create connection
 $conn = new mysqli($servername, $username, $password);
-
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
