a4f13e318b4a4c148362af18070c1cce8e59b1d0
Sajjad Ranjbar <sajjadranjbaryazdi@gmail.com>
2022-11-01 12:22:31 +0330

    Update state.php

--- state.php
+++ state.php
@@ -1,39 +1,85 @@
 <?php
-include("db_connect.php");
-$errors = [];
-$request = $_SERVER["REQUEST_METHOD"];
-$allowed_request = ["GET", "POST", "PUT", "DELETE"];
-if(isset($_POST['student_id']))
-{
-  if(in_array($request,$allowed_request))
-  {
-    if($request == 'POST')
-    {
-      $student_id = $_POST['student_id']; //Registered id of the student in the device
-      $date = $_POST['date']; //Recorded date YYYY/MM/DD
-      $time = $_POST['time']; //Recorded hours H:i:s
-      $type = $_POST['type']; //type of traffic (entry/exit)
-      $device_id = $_POST['device_id']; //Registered id of the device
-      
-      $sql = "INSERT INTO en_ex (student_id, date, time, type, device_id)
-      VALUES ($student_id, '$date', '$time', $type, $device_id)";
 
-      if ($conn->query($sql) === TRUE)
-      {
-        array_push($errors,"New record created successfully");
-        $data = array(
-          "success" => true,
-          "http_code" => 200
-        );
-      } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Methods: POST");
+    header("Access-Control-Max-Age: 3600");
+    header("X-Requested-With = XMLHttpRequest");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+    include("db_connect.php");
+    include("jdf.php");
+    $errors = [];
+    $request = $_SERVER["REQUEST_METHOD"];
+    $allowed_request = ["GET", "POST", "PUT", "DELETE"];
+
+
+if(isset($_GET['token']))
+{
+    $student_id = $_GET['token'];
+    $time = jdate("H:i:s");
+    $date = jdate("Y/m/d");
+    $device_id = 0;
+    $type = 0;
+    $confirm = 0;
+    $sql = "SELECT * FROM students";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+      while($row = $result->fetch_assoc()) {
+          if($_GET['token'] == $row['id_no']){
+              $name = $row['name'];
+              $confirm = 1;
+          }
       }
     }
+    if($confirm == 1){
+        if(in_array($request,$allowed_request)){
+           if($request == 'GET'){
+               //othing yet...
+               //echo "hello";
+               //echo $_GET['user_id'];
     
-  }
- }
+           } 
+           if($request == 'POST'){
+               //othing yet...
+               //echo "hi";
+               //echo $_GET['token'];
+               //echo $_POST['user_id'];
+               $sql2 = "INSERT INTO en_ex (student_id, device_id, date, time, type, name)VALUES ($student_id, $device_id, '$date', '$time', '$type', '$name')";
+                
+                if ($conn->query($sql2) === TRUE) {
+                    array_push($errors,"Recorrd created successfully!");
+                    $data = array(
+                    "success" => true,
+                    "http_code" => 200,
+                    "user_id" => $student_id,
+                    "name" => $name,
+                    "time" => "$time",
+                    "date" => "$date"
+                    );
+                } else {
+                  echo "Error: " . $sql . "<br>" . $conn->error;
+                }
+           } 
+        }
+        else{
+            array_push($errors,"our request is illegal!");
+            $data = array(
+            "success" => false,
+            "http_code" => 400
+            );
+        } 
+    }
+    else{
+        array_push($errors,"there is no student with this id!");
+            $data = array(
+            "success" => false,
+            "http_code" => 400
+            );
+    }
+
+}
 else{
-  array_push($errors,"'student_id' parameter is undefined!");
+  array_push($errors,"'token' parameter is undefined!");
   $data = array(
     "success" => false,
     "http_code" => 400
@@ -41,4 +87,5 @@ else{
 }
 if(count($errors) != 0) $data['$errors'] = $errors;
 echo json_encode($data);
+
 ?>
