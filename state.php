50efd3afbaf9e7d025064869bf3ee9f46b95a7c5
Sajjad Ranjbar <sajjadranjbaryazdi@gmail.com>
2022-12-07 19:15:49 +0330

    Update state.php

--- state.php
+++ state.php
@@ -6,77 +6,64 @@
     header("Access-Control-Max-Age: 3600");
     header("X-Requested-With = XMLHttpRequest");
     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-    include("db_connect.php");
+    
     include("jdf.php");
+    
     $errors = [];
     $request = $_SERVER["REQUEST_METHOD"];
     $allowed_request = ["GET", "POST", "PUT", "DELETE"];
 
 
-if(isset($_GET['token']))
+if(isset($_GET['student_id']))
 {
-    $student_id = $_GET['token'];
-    $time = jdate("H:i:s");
-    $date = jdate("Y/m/d");
-    $device_id = 0;
-    $type = 0;
-    $confirm = 0;
-    $sql = "SELECT * FROM students";
-    $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
-      while($row = $result->fetch_assoc()) {
-          if($_GET['token'] == $row['id_no']){
-              $name = $row['name'];
-              $confirm = 1;
-          }
-      }
+    $student_id = $_GET['student_id'];
+    $name = $_GET['name'];
+    $device_id = $_GET['device_id'];
+    $time = $_GET['time'];
+    $date = $_GET['date'];
+    //$date = jdate("Y/m/d");
+    if($device_id == 1){
+        include("db_connect.php");
     }
-    if($confirm == 1){
-        if(in_array($request,$allowed_request)){
-           if($request == 'GET'){
-               //othing yet...
-               //echo "hello";
-               //echo $_GET['user_id'];
     
-           } 
-           if($request == 'POST'){
-               //othing yet...
-               //echo "hi";
-               //echo $_GET['token'];
-               //echo $_POST['user_id'];
-               $sql2 = "INSERT INTO en_ex (student_id, device_id, date, time, type, name)VALUES ($student_id, $device_id, '$date', '$time', '$type', '$name')";
-                
-                if ($conn->query($sql2) === TRUE) {
-                    array_push($errors,"Recorrd created successfully!");
-                    $data = array(
-                    "success" => true,
-                    "http_code" => 200,
-                    "user_id" => $student_id,
-                    "name" => $name,
-                    "time" => "$time",
-                    "date" => "$date"
-                    );
-                } else {
-                  echo "Error: " . $sql . "<br>" . $conn->error;
-                }
-           } 
+    
+    $sql2 = "SELECT * FROM en_ex WHERE student_id='$student_id' AND date='$date'";
+    $result2 = $conn->query($sql2);
+    $confirm = 1;
+    if ($result2->num_rows > 0) {
+        while($row2 = $result2->fetch_assoc()) {
+            if($row2['time'] == $time)
+            {
+                $confirm = 0;
+                break;
+            }
         }
-        else{
-            array_push($errors,"our request is illegal!");
-            $data = array(
-            "success" => false,
-            "http_code" => 400
-            );
-        } 
+    }
+    
+    if($confirm == 1)
+    {
+        $sql = "INSERT INTO en_ex (student_id, device_id, date, time, name)VALUES ($student_id, $device_id, '$date', '$time', '$name')";
+        if ($conn->query($sql) === TRUE) {
+            array_push($errors,"Recorrd created successfuly!");
+              $data = array(
+                "success" => true,
+                "http_code" => 200,
+                "student_id" => $student_id,
+                "device_id" => $device_id,
+                "time" => $time,
+                "date" => $date,
+              );
+        } else {
+             echo "Error: " . $sql . "<br>" . $conn->error;
+        }
     }
     else{
-        array_push($errors,"there is no student with this id!");
-            $data = array(
+        array_push($errors,"Warmed over! Repeated data...");
+        $data = array(
             "success" => false,
             "http_code" => 400
-            );
+        );
     }
-
 }
 else{
   array_push($errors,"'token' parameter is undefined!");
